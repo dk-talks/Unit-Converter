a422acd3b3e7cea2dc26714ccfaeb91fdb388e38
dk-talks <dksbansur8502@gmail.com>
2023-04-04 10:08:20 +0530

    Initial Commit

---
+++ Ikai Unit Yantra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ikai Unit Yantra
+//
+//  Created by Dinesh Sharma on 04/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
